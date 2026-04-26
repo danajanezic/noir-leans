207a2e872cf93fd01af7fbfaec28cc829d2c1de7
Dana Jane Zic <danajanezic@gmail.com>
2026-04-26 15:16:06 -0700

    feat: change map trigger to /map

--- tests/test_map.py
+++ tests/test_map.py
@@ -41,10 +41,4 @@ def test_render_map_different_active_neighborhood():
 
 
 def test_parse_map_command():
-    assert parse_command("map").intent == Intent.MAP
-
-
-def test_parse_map_command_variants():
-    assert parse_command("show map").intent == Intent.MAP
-    assert parse_command("open map").intent == Intent.MAP
-    assert parse_command("view map").intent == Intent.MAP
+    assert parse_command("/map").intent == Intent.MAP
